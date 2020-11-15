b103918f2f7cc53bb864928a64dcc4000ab575a4
prathameshkoshti <prathu48@gmail.com>
2020-11-15 22:15:49 +0530

    Fix: footer alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <footer className="footer full-width">
-                Made with 💜 &nbsp;using sass.
+            <footer className="footer full-width justify-content-space-between align-items-center">
+                <span>Made with 💜 &nbsp;using sass.</span>
             </footer>
         )
     }
