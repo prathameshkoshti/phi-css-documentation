6710ca09197b0ef1ab1fe4b8367249af31fc4b94
prathameshkoshti <prathu48@gmail.com>
2020-11-15 22:17:36 +0530

    Feat: Added copy functionality for code block

--- src/components/PrismCodeBlock.js
+++ src/components/PrismCodeBlock.js
@@ -8,12 +8,32 @@ export default class PrismCodeBlock extends Component {
             language: this.props.language,
             code: this.props.code,
         }
+
+        this.handleCopyButton = this.handleCopyButton.bind(this);
+    }
+
+    handleCopyButton(event) {
+        const target = event.currentTarget;
+
+        // Copy text
+        navigator.clipboard.writeText(this.props.code);
+        this.updateText(target, 'Copied');
+        target.classList.add('copied');
+
+        setTimeout(() => {
+            this.updateText(target, 'Copy');
+            target.classList.remove('copied');
+        }, 2000);
+    }
+
+    updateText(target, value) {
+        target.innerText = value;
     }
 
     componentDidMount() {
         Prism.highlightAll();
     }
-
+ 
     render() {
         return (
             <div className="code-block">
@@ -22,8 +42,8 @@ export default class PrismCodeBlock extends Component {
                         {this.state.code}
                     </code>
                 </pre>
-                <div className="code-block-footer">
-                    {this.state.language}
+                <div onClick={(event) => this.handleCopyButton(event)} className="code-block-copy">
+                    Copy
                 </div>
             </div>
         )
