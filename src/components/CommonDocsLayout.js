db416378f3073e31918dd4ab5372d413fecfe247
prathameshkoshti <prathu48@gmail.com>
2020-11-21 12:14:58 +0530

    Added background for docs headings

--- src/components/CommonDocsLayout.js
+++ src/components/CommonDocsLayout.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import DocsSidebar from '../components/DocsSidebar/DocsSidebar';
 import Pagination from '../components/Pagination';
+import { docsBackground } from '../utils/docsBackgrounds';
 
 export default class CommonDocsLayout extends Component {
     render() {
+        let headerStyle = {};
+
+        if (this.props.currentPageId === 'dark_mode') {
+            headerStyle = {
+                color: 'var(--accent-tint)'
+            }
+        }
+
         return (
             <div className="docs-contianer">
-            <div className="page-header full-width">
-                <h3>{this.props.pageHeader}</h3>
+                <div className="page-header full-width">
+                    <h3 style={headerStyle}>{this.props.pageHeader}</h3>
+                    <div className="tint" style={docsBackground[this.props.currentPageId].tintStyle}></div>
+                    <div className="header-background" style={docsBackground[this.props.currentPageId].style}></div>
+                </div>
+                <div className="docs-content">
+                    <article>
+                        <div className="page-content">
+                            {this.props.children}
+                        </div>
+                    </article>
+                    <Pagination currentPageId={this.props.currentPageId} parentPageId={this.props.parentPageId} />
+                </div>
+                <div className="docs-sidebar-container">
+                    <DocsSidebar />
+                </div>
             </div>
-            <div className="docs-content">
-                <article>
-                    <div className="page-content">
-                        {this.props.children}
-                    </div>
-                </article>
-                <Pagination currentPageId={this.props.currentPageId} parentPageId={this.props.parentPageId} />
-            </div>
-            <div className="docs-sidebar-container">
-                <DocsSidebar />
-            </div>
-        </div>
         )
     }
 }
---
+++ src/utils/docsBackgrounds.js
@@ -0,0 +1,127 @@
+export const docsBackground = {
+    inception: {
+        style: {
+            backgroundColor: '#f8f8ff',
+            opacity: 1,
+            backgroundImage: 'radial-gradient(#7a65fb 1.5px, transparent 1.35px), radial-gradient(#7a65fb 1.5px, #f8f8ff 2px)',
+            backgroundSize: '60px 60px',
+            backgroundPosition: '0 0, 27px 27px',
+        },
+        tintStyle: {
+            opacity: 0.7,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    installation: {
+        style: {
+            backgroundColor: '#f8f8ff',
+            opacity: 1,
+            background: 'radial-gradient(circle, transparent 20%, #f8f8ff 20%, #f8f8ff 80%, transparent 80%, transparent), radial-gradient(circle, transparent 20%, #f8f8ff 20%, #f8f8ff 80%, transparent 80%, transparent) 15px 15px, linear-gradient(#7a65fb 1.2000000000000002px, transparent 1.2000000000000002px) 0 -0.6000000000000001px, linear-gradient(90deg, #7a65fb 1.2000000000000002px, #f8f8ff 1.2000000000000002px) -0.6000000000000001px 0',
+            backgroundSize: '25px 25px, 30px 30px, 15px 15px, 15px 15px',
+        },
+        tintStyle: {
+            opacity: 0.5,
+            backdropFilter: 'blur(3px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    base: {
+        style: {
+            background: 'radial-gradient(circle farthest-side at 0% 50%,#f8f8ff 23.5%,rgba(240,166,17,0) 0)21px 30px, radial-gradient(circle farthest-side at 0% 50%,#7a65fb 24%,rgba(240,166,17,0) 0)19px 30px, linear-gradient(#f8f8ff 14%,rgba(240,166,17,0) 0, rgba(240,166,17,0) 85%,#f8f8ff 0)0 0, linear-gradient(150deg,#f8f8ff 24%,#7a65fb 0,#7a65fb 26%,rgba(240,166,17,0) 0,rgba(240,166,17,0) 74%,#7a65fb 0,#7a65fb 76%,#f8f8ff 0)0 0, linear-gradient(30deg,#f8f8ff 24%,#7a65fb 0,#7a65fb 26%,rgba(240,166,17,0) 0,rgba(240,166,17,0) 74%,#7a65fb 0,#7a65fb 76%,#f8f8ff 0)0 0, linear-gradient(90deg,#7a65fb 2%,#f8f8ff 0,#f8f8ff 98%,#7a65fb 0%)0 0 #f8f8ff',
+            backgroundSize: '40px 60px',
+        },
+        tintStyle: {
+            opacity: 0.7,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    grid: {
+        style: {
+            backgroundColor: 'var(--light)',
+            opacity: 1,
+            backgroundImage:  'linear-gradient(var(--accent) 2.2px, transparent 2.2px), linear-gradient(90deg, var(--accent) 2.2px, transparent 2.2px), linear-gradient(var(--accent-tint) 1.1px, transparent 1.1px), linear-gradient(90deg, var(--accent-tint) 1.1px, var(--light) 1.1px)',
+            backgroundSize: '60px 60px, 60px 60px, 5px 5px, 5px 5px',
+            backgroundPosition: '-2.2px -2.2px, -2.2px -2.2px, -1.1px -1.1px, -1.1px -1.1px',
+        },
+        tintStyle: {
+            opacity: 0.75,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    typography: {
+        style: {
+            background: 'url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="1em" y="1em" font-weight="100">a</text></svg>\');',
+            backgroundSize: '75px 75px',
+        },
+        tintStyle: {
+            opacity: 0.8,
+            backdropFilter: 'blur(3px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    form_components: {
+        style: {
+            backgroundColor: 'var(--light)',
+            opacity: 1,
+            backgroundImage:  'linear-gradient(30deg, #7a65fb 12%, transparent 12.5%, transparent 87%, #7a65fb 87.5%, #7a65fb), linear-gradient(150deg, #7a65fb 12%, transparent 12.5%, transparent 87%, #7a65fb 87.5%, #7a65fb), linear-gradient(30deg, #7a65fb 12%, transparent 12.5%, transparent 87%, #7a65fb 87.5%, #7a65fb), linear-gradient(150deg, #7a65fb 12%, transparent 12.5%, transparent 87%, #7a65fb 87.5%, #7a65fb), linear-gradient(60deg, #7a65fb77 25%, transparent 25.5%, transparent 75%, #7a65fb77 75%, #7a65fb77), linear-gradient(60deg, #7a65fb77 25%, transparent 25.5%, transparent 75%, #7a65fb77 75%, #7a65fb77)',
+            backgroundSize: '36px 63px',
+            backgroundPosition: '0 0, 0 0, 18px 32px, 18px 32px, 0 0, 18px 32px',
+        },
+        tintStyle: {
+            opacity: 0.85,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    tables: {
+        style: {
+            backgroundColor: '#f8f8ff',
+            opacity: 1,
+            backgroundImage: 'linear-gradient(#7a65fb 2px, transparent 2px), linear-gradient(to right, #7a65fb 2px, #f8f8ff 2px)',
+            backgroundSize: '40px 40px',
+        },
+        tintStyle: {
+            opacity: 0.7,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    utilities: {
+        style: {
+            background: 'radial-gradient(#7a65fb 3px, transparent 4px), radial-gradient(#7a65fb 3px, transparent 4px), linear-gradient(#fff 4px, transparent 0), linear-gradient(45deg, transparent 74px, transparent 75px, #7a65fb 77px, #7a65fb 77px, transparent 78px, transparent 109px), linear-gradient(-45deg, transparent 75px, transparent 76px, #7a65fb 77px, #7a65fb 78px, transparent 78px, transparent 109px), #fff',
+            backgroundSize: '109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px',
+            backgroundPosition: '54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px'
+        },
+        tintStyle: {
+            opacity: 0.8,
+            backdropFilter: 'blur(2px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    },
+    dark_mode: {
+        style: {
+            background: 'var(--accent-shade)',
+            color: 'var(--accent-tint)',
+        },
+        tintStyle: {
+            opacity: 0.5,
+            backdroppFilter: 'blur(3px)',
+            backgroundImage: 'radial-gradient(circle at bottom left, #ccc4fd88 0%, transparent 40%)',
+        }
+    },
+    customizations: {
+        style: {
+            backgroundColor: '#f8f8ff',
+            opacity: 1,
+            backgroundImage: 'repeating-radial-gradient( circle at 0 0, transparent 0, #f8f8ff 24px ), repeating-linear-gradient( #7a65fb55, #7a65fb )',
+        },
+        tintStyle: {
+            opacity: 0.8,
+            backdropFilter: 'blur(5px)',
+            backgroundImage: 'radial-gradient(circle at top left, #ccc4fdBB 0%, transparent 50%)',
+        }
+    }
+}
