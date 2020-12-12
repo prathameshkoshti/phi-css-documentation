698edac7fce6d23821f58b1849bbe08bc0a20577
prathameshkoshti <prathu48@gmail.com>
2020-12-12 17:59:09 +0530

    Grammar changes and phi package updated

--- src/pages/docs/colors.js
+++ src/pages/docs/colors.js
@@ -7,7 +7,7 @@ export default function colors() {
         <CommonDocsLayout currentPageId="colors" pageHeader="Colors">
             <section>
                 <p>
-                    Only standard colors are defined and from the standard colors, tints and shades are generated using <code>$phi</code> value. Checkout the below table to see which colors are readily available. You can also use the CSS variables (a.k.a. CSS Custom Properties) to utilize these colors in your website.
+                    Only standard colors are defined and from the standard colors, tints and shades are generated using <code>$phi</code> value. Check out the below table to see which colors are readily available. You can also use the CSS variables (a.k.a. CSS Custom Properties) to utilize these colors on your website.
                 </p>
                 <ColorTable />
             </section>
