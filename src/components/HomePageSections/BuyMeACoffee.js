a4c1723bbb75b699f71eba5eb202bb5ad412375c
prathameshkoshti <prathu48@gmail.com>
2020-12-24 23:11:51 +0530

    added idea link to landing page

--- src/components/HomePageSections/BuyMeACoffee.js
+++ src/components/HomePageSections/BuyMeACoffee.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import Arrow from '../Arrow';
 import BuyMeCoffee from '../BuyMeCoffee';
+import Link from "next/link";
+
 export default function BuyMeACoffee() {
     return (
         <section className="buy-me-a-coffee-section">
@@ -11,7 +13,7 @@ export default function BuyMeACoffee() {
                         <Image className="my-image" src="/images/prathamshkoshti.png" alt="Prathamesh Koshti" width="100px" height="100px" />
                     </div>
                     <p className="text-center">
-                        Hey! I am <a href="https://prathameshkoshti.com/" className="link link-primary"><strong>Prathamesh Koshti</strong></a>. I'm the creator of this CSS framework, created this framework in my free time. I work as a full-time front end developer. If you do like the idea behind this framework, you can consider buying me a coffee, would be much helpful for me to maintain this project in future.
+                        Hey! I am <a href="https://prathameshkoshti.com/" className="link link-primary"><strong>Prathamesh Koshti</strong></a>. I'm the creator of this CSS framework, created this framework in my free time. I work as a full-time front end developer. If you do like <Link href="/idea"><a className="link link-primary"><strong>the idea</strong></a></Link> behind this framework, you can consider buying me a coffee, would be much helpful for me to maintain this project in future.
                     </p>
                 </div>
             </article>
